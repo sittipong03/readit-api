70f2c5974b5887045ea15c9ab819f95f53e8c6d3
sittipong03 <sittipong.f@gmail.com>
2025-07-24 13:22:26 +0700

    Main task : tags done , Minor change : prisma update , fix spell missing on app.js(orderRoute)

--- src/routes/book.route.js
+++ src/routes/book.route.js
@@ -6,4 +6,15 @@ const bookRoute = express.Router()
 
 bookRoute.get('/' ,bookController.testGet)
 
+
+
+
+
+
+// tags
+bookRoute.get('/tags' , bookController.getTags)
+bookRoute.post('/tags' , bookController.createTag)
+bookRoute.patch('/tags/:id', bookController.updateTag)
+bookRoute.delete('/tags/:id' , bookController.deleteTag)
+
 export default bookRoute
